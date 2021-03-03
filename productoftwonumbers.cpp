274d5df5b154481aba2e3e8263cd6bc4aec7dd87
PoonamSambhajiPawar <78857256+PoonamSambhajiPawar@users.noreply.github.com>
2021-03-03 20:34:41 +0530

    productoftwonumbers.cpp

---
+++ productoftwonumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b,product;
+    cout<<"Enter first number : ";
+    cin>>a;
+    cout<<"Enter second number : ";
+    cin>>b;
+    product=a*b;
+    cout<<"Product of two numbers is : "<<product<<endl;
+
+    return 0;
+}
